608acbacaa98cb75c93ecfbd13be6241206e82b6
jynoh00 <wndus123sh@naver.com>
2025-11-24 13:46:54 +0900

    re-Solve: test5 ~ test7, remind & modify XOR test

---
+++ new/xor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 0;
+    int b = 1;
+    int c = a ^ b;
+
+    cout << c << endl;
+    
+    return 0;
+}
+
+// rotlqkf; dkfrhdlTejsrjsep dho ;;;; tqtq
---
+++ re_solve/test7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void input();
+void solve();
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    input();
+    solve();
+
+    return 0;
+}
+
+void input() {
+
+}
+
+void solve() {
+
+}
